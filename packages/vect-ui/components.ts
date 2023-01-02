781d4ec526ea19b516048a1d79354ca9061d27f9
din <amifed.me@gmail.com>
2023-01-02 15:11:47 +0800

    ♻️refactor: update components export

--- packages/vect-ui/components.ts
+++ packages/vect-ui/components.ts
@@ -1,16 +1,13 @@
 import type { Plugin } from 'vue'
 
-import {
-  VBreadcrumb,
-  VBreadcrumbItem,
-  VButton,
-  VIcon,
-  VLink,
-  VRadio,
-  VRadioGroup,
-  VStep,
-  VSteps
-} from '@vect-ui/components'
+import { VButton } from '@vect-ui/components/Button'
+import { VBreadcrumb } from '@vect-ui/components/Breadcrumb'
+import { VBreadcrumbItem } from '@vect-ui/components/BreadcrumbItem'
+import { VIcon } from '@vect-ui/components/Icon'
+import { VLink } from '@vect-ui/components/Link'
+import { VRadio } from '@vect-ui/components/Radio'
+import { VRadioGroup } from '@vect-ui/components/RadioGroup'
+import { VStep, VSteps } from '@vect-ui/components/Steps'
 
 export default [
   VButton,
--- packages/vect-ui/index.ts
+++ packages/vect-ui/index.ts
@@ -1,5 +1,4 @@
 import plugin from './vuePlugin'
 
 export * from '@vect-ui/components'
-
 export default plugin
