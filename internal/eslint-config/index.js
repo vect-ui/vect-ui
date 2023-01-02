f6d040e43577c561619d4680d83444ab267a4129
din <amifed.me@gmail.com>
2023-01-02 12:54:27 +0800

    chore: update repo config

--- internal/eslint-config/index.js
+++ internal/eslint-config/index.js
@@ -71,7 +71,62 @@ module.exports = defineConfig({
         'no-undef': 'off'
       }
     },
-
+    {
+      files: ['package.json'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'jsonc/sort-keys': [
+          'error',
+          {
+            pathPattern: '^$',
+            order: [
+              'name',
+              'version',
+              'private',
+              'packageManager',
+              'description',
+              'type',
+              'keywords',
+              'homepage',
+              'bugs',
+              'license',
+              'author',
+              'contributors',
+              'funding',
+              'files',
+              'main',
+              'module',
+              'exports',
+              'unpkg',
+              'jsdelivr',
+              'browser',
+              'bin',
+              'man',
+              'directories',
+              'repository',
+              'publishConfig',
+              'scripts',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'optionalDependencies',
+              'dependencies',
+              'devDependencies',
+              'engines',
+              'config',
+              'overrides',
+              'pnpm',
+              'husky',
+              'lint-staged',
+              'eslintConfig'
+            ]
+          },
+          {
+            pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
+            order: { type: 'asc' }
+          }
+        ]
+      }
+    },
     {
       files: ['**/*.md/*.js', '**/*.md/*.ts'],
       rules: {
